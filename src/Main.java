7aed52ddcb7d61ad7f077c56c46b6e526f0fb622
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-08-16 19:45:08 +0300

    feat: add Main

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import myArrayList.MyArray;
+import myArrayList.MyArrayList;
+import myLinkedList.MyLinkedList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        MyLinkedList<Integer> list = new MyLinkedList<>();
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//
+//        System.out.println("Размер: " + list.size());  //  3
+//        System.out.println("Элемент по 1 индексу: " + list.getElementByIndex(1));  //  20
+//
+//        Integer removedElement = list.remove(1);
+//        System.out.println("Удаленный элемент: " + removedElement);  //  20
+//
+//        System.out.println("Размер после удаления: " + list.size());  //  2
+
+//       MyArray <String> test = new MyArrayList<>();
+//        test.add("first");
+//        test.add("second");
+//        test.add("third");
+//        test.add("fourth");
+//        test.add("fifth");
+//        test.add("sixth");
+//
+//        System.out.println(test.size());
+//        System.out.println(test.get(1));
+//        System.out.println(test.remove(1));
+//        System.out.println(test.size());
+//        System.out.println(test.set(1, "2"));
+//        System.out.println(test.get(1));
+
+    }
+}
