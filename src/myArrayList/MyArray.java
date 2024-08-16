7eda59ea3de8972a774439613c292802b8c872da
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-08-16 14:26:50 +0300

    refactor: change return value, void=>E

--- src/myArrayList/MyArray.java
+++ src/myArrayList/MyArray.java
@@ -2,7 +2,7 @@ package myArrayList;
 
 public interface MyArray<E> extends Iterable<E> {
     boolean add(E el);
-    void remove(int index);
+    E remove(int index);
     E get(int index);
     E set(int index, E el);
     int size();
